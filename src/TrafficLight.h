a677b4104c3d1b7f89c5cdb83969016a02ea0671
milan-r-shah <mrshah@wpi.edu>
2020-04-27 17:14:54 -0400

    updated MessageQueue template class in TrafficLight.h for Task FP.3

--- src/TrafficLight.h
+++ src/TrafficLight.h
@@ -24,9 +24,13 @@ template <class T>
 class MessageQueue
 {
 public:
+    void send(T &&msg);
+    T receive();
 
 private:
-    
+    std::deque<T> _queue;
+    std::condition_variable _cvMsgQ;
+    std::mutex _mtxMsgQ;
 };
 
 // FP.1 : Define a class „TrafficLight“ which is a child class of TrafficObject. 
